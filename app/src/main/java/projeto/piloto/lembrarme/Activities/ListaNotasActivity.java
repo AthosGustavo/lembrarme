2e09bbeac5467400cd2196507cae0b76c86cff95
Athos Gustavo <athosgustavowf@gmail.com>
2025-03-15 14:25:29 -0300

    Estilização básica dos itens do recyclerview

--- app/src/main/java/projeto/piloto/lembrarme/Activities/ListaNotasActivity.java
+++ app/src/main/java/projeto/piloto/lembrarme/Activities/ListaNotasActivity.java
@@ -1,13 +1,10 @@
-package projeto.piloto.lembrarme;
+package projeto.piloto.lembrarme.Activities;
 
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import projeto.piloto.lembrarme.R;
 
 public class ListaNotasActivity extends AppCompatActivity {
 
