5b74fc33880a479823205f48b1075c6b41a53e08
Athos Gustavo <athosgustavowf@gmail.com>
2025-03-15 13:03:04 -0300

    Layout do básico do RecyclerView adicionado

---
+++ app/src/main/java/projeto/piloto/lembrarme/ListaNotasActivity.java
@@ -0,0 +1,22 @@
+package projeto.piloto.lembrarme;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class ListaNotasActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_lista_notas);
+
+  }
+
+
+}
